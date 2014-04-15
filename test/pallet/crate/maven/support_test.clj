a39669174fa2af04e38da4a5cae17f4f193c5d06
Hugo Duncan <hugo@hugoduncan.org>
2014-04-15 17:22:55 -0400

    Update support test to use with-group-spec

--- test/pallet/crate/maven/support_test.clj
+++ test/pallet/crate/maven/support_test.clj
@@ -3,7 +3,7 @@
    [clojure.test :refer :all]
    [pallet.actions :refer [exec-checked-script exec-script* minimal-packages
                            package-manager]]
-   [pallet.api :refer [converge group-spec plan-fn]]
+   [pallet.api :refer [group-spec lift plan-fn]]
    [pallet.build-actions :refer [build-actions build-session]]
    [pallet.core.api :refer [phase-errors]]
    [pallet.core.session :refer [with-session]]
@@ -12,22 +12,24 @@
    [pallet.crate.java :as java]
    [pallet.crate.maven :as maven]
    [pallet.crates.test-nodes :as test-nodes]
+   [pallet.repl :refer [explain-session]]
    [pallet.script :refer [with-script-context]]
    [pallet.script-test :refer [is-true testing-script]]
    [pallet.script.lib :refer [package-manager-non-interactive]]
-   [pallet.test-env :refer [teardown test-env *compute-service* *node-spec-meta*]]
+   [pallet.test-env
+    :refer [*compute-service* *node-spec-meta* test-env
+            unique-name with-group-spec ]]
    [pallet.test-env.project :as project]))
 
-(test-env test-nodes/node-specs project/project {:threads 10})
+(test-env test-nodes/node-specs project/project)
 
 (deftest ^:support default-settings
-  (let [spec (group-spec
-                 (keyword (str "pallet-maven-" (name (:selector *node-spec-meta*))))
+  (let [spec (group-spec (keyword (unique-name))
                :node-spec (:node-spec *node-spec-meta*)
                :count 1
                :phases
                {:bootstrap (plan-fn
-                               (minimal-packages)
+                             (minimal-packages)
                              (package-manager :update)
                              (automated-admin-user))
                 :settings (plan-fn
@@ -39,20 +41,18 @@
                              (java/install {})
                              (maven/install {}))
                 :verify (plan-fn
-                            (exec-script*
-                             (testing-script
-                              "Maven installed"
-                              (is-true ("mvn" -version "2>/dev/null")
-                                       "Verify maven is installed"))))})]
+                          (exec-script*
+                           (testing-script
+                            "Maven installed"
+                            (is-true ("mvn" -version "2>/dev/null")
+                                     "Verify maven is installed"))))})]
 
-    (try
-      (let [session (converge spec
-                              :phase [:configure :verify]
-                              :compute *compute-service*)]
+    (with-group-spec spec
+      (let [session (lift spec
+                          :phase [:configure :verify]
+                          :compute *compute-service*)]
         (testing "install maven"
           (is session)
-          (is (not (phase-errors session)))))
-      (finally
-        (teardown
-         (converge (assoc spec :count 0) :compute *compute-service*))
-        ))))
+          (is (not (phase-errors session)))
+          (when (phase-errors session)
+            (explain-session session)))))))
